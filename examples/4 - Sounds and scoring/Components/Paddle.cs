a776cd2dbe53e4d41abb1058e459e30cfe8cc0a5
kofu145 <kofu145g@gmail.com>
2024-01-01 16:59:15 -0800

    chapter 4 example files

---
+++ examples/4 - Sounds and scoring/Components/Paddle.cs
@@ -0,0 +1,81 @@
+﻿using System.Numerics;
+using GramEngine.Core;
+using GramEngine.ECS;
+using GramEngine.Core.Input;
+using GramEngine.ECS.Components;
+
+namespace GramPong.Components;
+
+public enum Side
+{
+    Left,
+    Right
+}
+
+public class Paddle : Component
+{
+    public Side PaddleSide;
+    private float speed;
+    private float velocity;
+    public Vector2 PaddleSize;
+    public int Score;
+
+    // The set keybinds to use to control either side.
+    private Keys[] leftPlayerInputs = { Keys.W, Keys.S};
+    private Keys[] rightPlayerInputs = { Keys.Up, Keys.Down };
+    public Paddle(Side side, float speed)
+    {
+        PaddleSide = side;
+        this.speed = speed;
+        Score = 0;
+    }
+    
+    public override void Initialize()
+    {
+        base.Initialize();
+        // We store the paddle size so that we can use it when checking collisions in the ball component
+        // (otherwise, we'd have to call getcomponent<sprite>() per frame)
+        var paddleWidth = ParentEntity.GetComponent<Sprite>().Width;
+        var paddleHeight = ParentEntity.GetComponent<Sprite>().Height;
+
+        // We also want to adjust the paddle size for any scaling we might have done
+        PaddleSize = new Vector2(paddleWidth * Transform.Scale.X, 
+            paddleHeight * Transform.Scale.Y);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    
+        // Basic switch statements that moves each paddle to its accordingly bound keys, clamped to screen bounds.
+        velocity = 0f;
+
+        switch (PaddleSide)
+        {
+            case Side.Left:
+                if (InputManager.GetKeyPressed(leftPlayerInputs[0]))
+                    velocity = -speed;
+                if (InputManager.GetKeyPressed(leftPlayerInputs[1]))
+                    velocity = speed;
+                break;
+        
+            case Side.Right:
+                if (InputManager.GetKeyPressed(rightPlayerInputs[0]))
+                    velocity = -speed;
+                if (InputManager.GetKeyPressed(rightPlayerInputs[1]))
+                    velocity = speed;
+                break;
+        
+            default:
+                break;
+        }
+
+        Transform.Position.Y += velocity * gameTime.DeltaTime;
+
+        var yOffset = PaddleSize.Y / 2;
+        Transform.Position.Y = Math.Clamp(Transform.Position.Y, yOffset, 
+            GameStateManager.Window.settings.BaseWindowHeight - yOffset);
+
+    }
+    
+}
---
+++ examples/4 - Sounds and scoring/MainScene.cs
@@ -0,0 +1,86 @@
+﻿using System.Numerics;
+using GramEngine.Core;
+using GramEngine.ECS;
+using GramEngine.ECS.Components;
+using GramPong.Components;
+
+namespace GramPong;
+
+public class MainScene : GameState
+{
+    
+    public const float PaddleSize = 7f;
+    public const float BallSize = 5f;
+    
+    public const float BallSpeed = 300f;
+    public const float PaddleSpeed = 500f;
+    
+    private Entity ball;
+    private Entity leftPlayer;
+    private Entity rightPlayer;
+    private Entity ballCollisionSound;
+    private Entity ballScoreSound;
+    private Entity scoreKeeperEntity;
+
+    public override void Initialize()  
+    {  
+        base.Initialize();
+        ball = new Entity();  
+        leftPlayer = new Entity();  
+        rightPlayer = new Entity();
+        ballCollisionSound = new Entity();
+        ballScoreSound = new Entity();
+        scoreKeeperEntity = new Entity();
+        
+        leftPlayer.Tag = "paddle";
+        rightPlayer.Tag = "paddle";
+        
+        ball.AddComponent(new Sprite("Content/ball.png"));
+        leftPlayer.AddComponent(new Sprite("Content/paddle.png"));
+        rightPlayer.AddComponent(new Sprite("Content/paddle.png"));
+        
+        ballCollisionSound.AddComponent(new Sound("Content/blipSelect.wav"));
+        ballScoreSound.AddComponent(new Sound("Content/score.wav"));
+        
+        leftPlayer.AddComponent(new Paddle(Side.Left, PaddleSpeed));
+        rightPlayer.AddComponent(new Paddle(Side.Right, PaddleSpeed));
+        ball.AddComponent(new Ball(12.5f, BallSpeed, 
+            ballCollisionSound.GetComponent<Sound>(), 
+            ballScoreSound.GetComponent<Sound>()));
+
+        scoreKeeperEntity.AddComponent(new TextComponent("text", "Content/square.ttf", 50));
+        
+        ball.Transform.Scale = new Vector2(BallSize, BallSize);
+        leftPlayer.Transform.Scale = new Vector2(PaddleSize, PaddleSize);
+        rightPlayer.Transform.Scale = new Vector2(PaddleSize, PaddleSize);
+        
+        var paddleWidth = leftPlayer.GetComponent<Sprite>().Width;
+        var paddleHeight = leftPlayer.GetComponent<Sprite>().Height;
+        
+        ball.Transform.Position = new Vector3(300f, 200f, 1f);
+        leftPlayer.Transform.Position = new Vector3(paddleWidth * PaddleSize / 2, 
+            GameStateManager.Window.Height / 2, 
+            1f
+            );
+        rightPlayer.Transform.Position = new Vector3(
+            GameStateManager.Window.Width - paddleWidth * PaddleSize / 2, 
+            GameStateManager.Window.Height / 2, 
+            1f
+        );
+        scoreKeeperEntity.Transform.Position = new Vector3(260, 20, 1f);
+        
+        AddEntity(leftPlayer);
+        AddEntity(rightPlayer);
+        AddEntity(ball);
+        AddEntity(ballCollisionSound);
+        AddEntity(ballScoreSound);
+        AddEntity(scoreKeeperEntity);
+    }
+    
+    public override void Update(GameTime gameTime)  
+    {  
+        base.Update(gameTime);
+        scoreKeeperEntity.GetComponent<TextComponent>().Text = 
+            $"{leftPlayer.GetComponent<Paddle>().Score} | {rightPlayer.GetComponent<Paddle>().Score}";
+    }
+}
